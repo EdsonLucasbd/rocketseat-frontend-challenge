fff9528c67f98ce03710846aaa7d45882cebcfd7
Edson Lucas <47072312+EdsonLucasbd@users.noreply.github.com>
2023-08-08 16:31:46 -0300

    adjustment of "empty cart" illustration

--- rocketseat-test/src/components/EmptyCart/index.tsx
+++ rocketseat-test/src/components/EmptyCart/index.tsx
@@ -1,14 +1,24 @@
+import { ShoppingBagOpen } from '@phosphor-icons/react'
 import Image from 'next/image'
+import Link from 'next/link'
 
 export const EmptyCart = () => {
   return (
-    <div className='mt-10 w-[46rem] opacity-50'>
+    <div className='flex flex-col items-center justify-center gap-8 mt-10 w-[46rem]'>
       <Image
         src={'/EmptyShopCart.svg'}
-        width={1597}
-        height={785}
+        width={497}
+        height={45}
         alt=''
+        className='opacity-50'
       />
+      <p className='font-semibold text-xl text-color-text'>Seu carrinho está vazio</p>
+      <Link href='/' className="flex items-center justify-center gap-3 w-1/2 h-11 rounded text-background bg-brand-blue
+      hover:bg-brand-blue/90"
+      >
+        <ShoppingBagOpen className="w-6 h-6" />
+        Continuar comprando
+      </Link>
     </div>
   )
 }
